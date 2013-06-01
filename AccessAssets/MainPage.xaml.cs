a45602a177b9b2c872afbe2d576e5e658159d699
破船 <BeyondVincent@gmail.com>
2013-06-02 01:14:54 +0800

    add AccessAssets project

---
+++ AccessAssets/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using System.Xml.Linq;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DevDiv_AccessAssets
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            LoadCountriesXML();
+        }
+
+public async void LoadCountriesXML()
+{
+    string CountriesFile = @"Assets\1.xml";
+    StorageFolder InstallationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+    StorageFile file = await InstallationFolder.GetFileAsync(CountriesFile);
+    Stream Countries = await file.OpenStreamForReadAsync();
+
+    XDocument xDOC = XDocument.Load(Countries);
+
+    testXml.Text = xDOC.ToString();
+}
+    }
+}
