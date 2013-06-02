875114c06b6cc58901fda685f0a514e21e211738
破船 <BeyondVincent@gmail.com>
2013-06-02 20:55:56 +0800

    add DataBinding project

---
+++ DataBinding/Data/ForumItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DevDiv_DataBinding.Data
+{
+    class ForumItem : INotifyPropertyChanged
+    {
+        private string name, link, info;// 论坛板块的名称,链接和描述信息
+        private int topicCount; // 主题数
+        public string Name
+        {
+            get { return name; }
+            set { name = value; NotifyPropertyChanged("Name"); }
+        }
+        public string Link
+        {
+            get { return link; }
+            set { link = value; NotifyPropertyChanged("Link"); }
+        }
+        public string Info
+        {
+            get { return info; }
+            set { info = value; NotifyPropertyChanged("info"); }
+        }
+        public int TopicCount
+        {
+            get { return topicCount; }
+            set { topicCount = value; NotifyPropertyChanged("TopicCount"); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
---
+++ DataBinding/Data/ViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace DevDiv_DataBinding.Data
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<ForumItem> forumItemList;
+        private int selectedItemIndex;
+        private string itemDetail;
+        public ViewModel()
+        {
+            forumItemList = new ObservableCollection<ForumItem>();
+            selectedItemIndex = -1;
+        }
+
+        public int SelectedItemIndex
+        {
+            get { return selectedItemIndex; }
+            set
+            {
+                selectedItemIndex = value; NotifyPropertyChanged("SelectedItemIndex");
+            }
+        }
+
+        public string ItemDetail
+        {
+            get { return itemDetail; }
+            set
+            {
+                itemDetail = value; NotifyPropertyChanged("ItemDetail");
+            }
+        }
+
+        public ObservableCollection<ForumItem> ForumItemList
+        {
+            get
+            {
+                return forumItemList;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+
+}
