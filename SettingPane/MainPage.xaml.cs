797da487b02171ff33780f185a6e2aa09834076b
破船 <BeyondVincent@gmail.com>
2013-06-03 20:01:20 +0800

    add SettingPane project

---
+++ SettingPane/MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ApplicationSettings;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DevDiv_SettingPaneDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Rect _windowBounds;
+        double _settingsWidth = 346;
+        Popup _settingsPopup;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            _windowBounds = Window.Current.Bounds;
+
+            Window.Current.SizeChanged += OnWindowSizeChanged;
+
+            SettingsPane.GetForCurrentView().CommandsRequested += BlankPage_CommandsRequested;
+        }
+
+        void OnWindowSizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            _windowBounds = Window.Current.Bounds;
+        }
+
+        void BlankPage_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            SettingsCommand cmd = new SettingsCommand("login", "登录", (x) =>
+            {
+                _settingsPopup = new Popup();
+                _settingsPopup.Closed += OnPopupClosed;
+                Window.Current.Activated += OnWindowActivated;
+                _settingsPopup.IsLightDismissEnabled = true;
+                _settingsPopup.Width = _settingsWidth;
+                _settingsPopup.Height = _windowBounds.Height;
+
+                SimpleSettingsNarrow mypane = new SimpleSettingsNarrow();
+                mypane.Width = _settingsWidth;
+                mypane.Height = _windowBounds.Height;
+
+                _settingsPopup.Child = mypane;
+                _settingsPopup.SetValue(Canvas.LeftProperty, _windowBounds.Width - _settingsWidth);
+                _settingsPopup.SetValue(Canvas.TopProperty, 0);
+                _settingsPopup.IsOpen = true;
+            });
+
+            args.Request.ApplicationCommands.Add(cmd);
+
+            SettingsCommand cmd1 = new SettingsCommand("logout", "注销", (x) =>
+            {
+            });
+
+            args.Request.ApplicationCommands.Add(cmd1);
+        }
+
+        private void OnWindowActivated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated)
+            {
+                _settingsPopup.IsOpen = false;
+            }
+        }
+
+        void OnPopupClosed(object sender, object e)
+        {
+            Window.Current.Activated -= OnWindowActivated;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SettingsPane.Show();
+        }
+
+        private void HandleSizeChange(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            _settingsWidth = Convert.ToInt32(rb.Content);
+        }
+    }
+}
