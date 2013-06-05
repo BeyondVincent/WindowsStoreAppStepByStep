ae860e9b241201583ca9d4d1c6a21695778c799e
破船 <BeyondVincent@gmail.com>
2013-06-05 21:50:07 +0800

    add SemanticZoom project

---
+++ SemanticZoom/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BV_SemanticZoom
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        StoreData _storeData = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _storeData = new StoreData();
+
+            List<GroupInfoList<object>> dataLetter = _storeData.GetGroupsByLetter();
+            cvs2.Source = dataLetter;
+            (semanticZoom.ZoomedOutView as ListViewBase).ItemsSource = cvs2.View.CollectionGroups;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void semanticZoom_ViewChangeStarted_1(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+
+        }
+    }
+}
