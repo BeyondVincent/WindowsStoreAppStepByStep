34cfb5b3b9a9ddb7de45df9debb506d6f2edd661
破船 <BeyondVincent@gmail.com>
2013-06-06 21:03:09 +0800

    add CustomizeMessageDialog project

---
+++ CustomizeMessageDialog/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BV_CustomizeMessageDialog
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MyUserControl control = new MyUserControl();
+            control.Width = Window.Current.Bounds.Width;
+            control.Height = Window.Current.Bounds.Height;
+
+            popup.Child = control;
+
+            popup.IsOpen = true;
+        }
+
+        private void popup_Closed_1(object sender, object e)
+        {
+            // 当Popup被关闭后，该函数会被调用，我们可以在这里做一些处理
+        }
+    }
+}
---
+++ CustomizeMessageDialog/MyUserControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BV_CustomizeMessageDialog
+{
+    public sealed partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            (this.Parent as Popup).IsOpen = false;
+        }
+    }
+}
